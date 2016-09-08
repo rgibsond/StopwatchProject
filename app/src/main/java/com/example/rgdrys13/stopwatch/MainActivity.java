e6a5a823727a4ece576c0398c640d781256dfe66
rgibsond <r.gibson.drysdale@gmail.com>
2016-09-08 11:37:07 -0400

    first commit from class 9/8/2016

---
+++ app/src/main/java/com/example/rgdrys13/stopwatch/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.rgdrys13.stopwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    //instance data
+    TextView count;
+    Button start, reset;
+    Timer t;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.count = (TextView) findViewById(R.id.counter_id);
+        this.start = (Button) findViewById(R.id.start);
+
+        this.t = new Timer();
+
+        this.start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                t.scheduleAtFixedRate( , 0, 1000);
+            }
+        });
+    }
+
+    class CounterTask extends TimerTask {
+
+        //instance data
+        long count;
+        //constructor
+        public CounterTask(long i){
+            this.count = i;
+        }
+
+        @Override
+        public void run() {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    MainActivity.this.count.setText(Long.toString(count++));
+                }
+            });
+        }
+    }
+}
